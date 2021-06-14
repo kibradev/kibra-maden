5c7e6df5ab2ea0f328dfd5c2a10247af0fed8810
kibra <62299912+kibradev@users.noreply.github.com>
2021-06-14 23:12:51 +0300

    Update server.lua

--- kibra-maden/server.lua
+++ kibra-maden/server.lua
@@ -2,6 +2,13 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		Citizen.Wait(5000)
+		print('[^2kibra:maden^0] - ^1Started^0')
+	end
+end)
+
 RegisterServerEvent("kibra-maden:kazmaSatinAldim")
 AddEventHandler("kibra-maden:kazmaSatinAldim", function()
 	local src = source
